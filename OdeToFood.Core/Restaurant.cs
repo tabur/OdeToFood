9bead985538cdbc7f878da899559d51362e056a7
tabur <tabulae@gmail.com>
2020-03-24 17:40:43 +0200

    Added a project and class to represent restaurants

---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public enum CuisineType
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+
+    }
+}
